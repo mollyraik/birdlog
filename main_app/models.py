d3af152b61f31c60f24c75191f152b964a08d2ff
mollyraik <molly.raik@gmail.com>
2023-04-02 18:12:22 -0400

    wip: defines bird model, migrates, connects to admin

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
+from datetime import datetime
+
+def get_current_time():
+    return datetime.now().time()
 
 # Create your models here.
+class Bird(models.Model):
+    species = models.CharField(max_length=100)
+    date_observed = models.DateField(default=timezone.now)
+    time_observed = models.TimeField(default=get_current_time)
+    location_observed = models.CharField(max_length=100)
+    weather = models.CharField(max_length=100)
+    number_observed = models.IntegerField(default=1)
+    field_notes = models.TextField(max_length=250)
+
+    def __str__(self):
+        return self.species
